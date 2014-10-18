58ebf924a886b7e211ccb0b6cf6171d1c724d043
Roman Senn <roman.l.senn@gmail.com>
2014-10-18 21:38:41 +0200

    ...

--- build/gnu/autogen.sh
+++ build/gnu/autogen.sh
@@ -5,27 +5,26 @@
 thisdir=`dirname "$0"`
 topdir=$thisdir/../..
 
-cd "$topdir"
+cd "${1-$topdir}"
 
 set -x
 
 libtoolize --force --copy --automake
-rm -f aclocal.m4; aclocal  -I m4 -I build/gnu
+rm -f aclocal.$topdir/m4; aclocal  $(test -d $topdir/m4 && echo -I$topdir/m4) $(test -d $topdir/build/gnu && echo -I$topdir/build/gnu)
 autoheader --force
 automake --force --copy --foreign --add-missing --foreign
-rm -f aclocal.m4; aclocal -I m4 -I build/gnu
-autoconf --force -I m4 -I build/gnu
+rm -f aclocal.$topdir/m4; aclocal $(test -d $topdir/m4 && echo -I$topdir/m4) $(test -d $topdir/build/gnu && echo -I$topdir/build/gnu)
+autoconf --force  $(test -d $topdir/m4 && echo -I$topdir/m4) $(test -d $topdir/build/gnu && echo -I$topdir/build/gnu)
 
 
 subdir() {
-	echo "Entering directory $1 ..." 1>&2
-	(cd "$1" && exec ${BASH:-sh} ${2-autogen.sh}) || RET=$?
-	echo "Leaving directory $1 ..." 1>&2
-	return $RET
+	if [ -d "$1" -a -f "$1/$2" ]; then
+		echo "Entering directory $1 ..." 1>&2
+		(cd "$1" && exec ${BASH:-sh} ${2-autogen.sh} .) || RET=$?
+		echo "Leaving directory $1 ..." 1>&2
+  fi
+	return ${RET-1}
 }
 
-subdir libtar ../build/gnu/autogen.sh || exit $?
-subdir libswsh build/gnu/autogen.sh || exit $?
-#([ -d objconv ] && cd objconv && exec sh -x autogen.sh) || exit $?
-#(cd libtar && exec ${BASH:-sh} autogen.sh) || exit $?
-#(cd libswsh && exec ${BASH:-sh} build/gnu/autogen.sh) || exit $?
+subdir libtar ../$topdir/build/gnu/autogen.sh || exit $?
+subdir libswsh $topdir/build/gnu/autogen.sh || exit $?
