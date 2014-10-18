cba5992756609e01ca267d1b349153b09f2a10ac
Roman Senn <roman.l.senn@gmail.com>
2014-10-18 21:27:51 +0200

    ...

--- build/gnu/autogen.sh
+++ build/gnu/autogen.sh
@@ -16,6 +16,16 @@ automake --force --copy --foreign --add-missing --foreign
 rm -f aclocal.m4; aclocal -I m4 -I build/gnu
 autoconf --force -I m4 -I build/gnu
 
+
+subdir() {
+	echo "Entering directory $1 ..." 1>&2
+	(cd "$1" && exec ${BASH:-sh} ${2-autogen.sh}) || RET=$?
+	echo "Leaving directory $1 ..." 1>&2
+	return $RET
+}
+
+subdir libtar ../build/gnu/autogen.sh || exit $?
+subdir libswsh build/gnu/autogen.sh || exit $?
 #([ -d objconv ] && cd objconv && exec sh -x autogen.sh) || exit $?
-(cd libtar && exec ${BASH:-sh} autogen.sh) || exit $?
-(cd libswsh && exec ${BASH:-sh} build/gnu/autogen.sh) || exit $?
+#(cd libtar && exec ${BASH:-sh} autogen.sh) || exit $?
+#(cd libswsh && exec ${BASH:-sh} build/gnu/autogen.sh) || exit $?
